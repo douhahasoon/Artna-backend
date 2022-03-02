79319bbfe93e4e8f0f94dce8984001e06d1ea8db
Douha Hasoon <33440905+douhahasoon@users.noreply.github.com>
2022-03-03 00:22:09 +0300

    final

--- commerce/admin.py
+++ commerce/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
 
-from commerce.models import Product,  ProductImage, Category,Order,Item, OrderStatus, City,Address, artist, Label
+from commerce.models import Product,  ProductImage, Category,Order, OrderStatus, City,Address, artist, Label
 
 
 class InlineProductImage(admin.TabularInline):
@@ -14,12 +14,12 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ('name', 'qty', 'description', 'cost', 'price', 'discounted_price')
 
 admin.site.register(Product ,ProductAdmin)
-# admin.site.register(ProductImage)
+#admin.site.register(ProductImage)
 admin.site.register(Category)
 admin.site.register(artist)
 admin.site.register(Label)
 admin.site.register(Order)
-admin.site.register(Item)
+
 admin.site.register( OrderStatus)
 admin.site.register( City)
 admin.site.register(Address)
